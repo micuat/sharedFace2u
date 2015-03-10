61c77392eb4f7331e5b401294c542761d63daf0d
naoto <micuat@aim.com>
2015-03-10 16:03:29 -0400

    Receive osc from osceleton and generate mesh

---
+++ Assets/Scripts/FaceGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityOSC;
+
+public class FaceGenerator : MonoBehaviour {
+
+	private int FACE_NUM_POINTS = 121;
+	public GameObject faceMesh;
+
+	// Use this for initialization
+	void Start () {
+		OSCHandler.Instance.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OSCHandler.Instance.UpdateLogs();
+		var servers = OSCHandler.Instance.Servers;
+		
+		foreach(var packet in servers["osceleton"].packets)
+		{
+			Debug.Log(packet.Address);	
+			if(packet.Address.Equals("/osceleton/face_mesh")) {
+				var list = new List<Vector3>();
+				for( int i = 0; i < FACE_NUM_POINTS; i++) {
+					float x = -(float)packet.Data[i * 3 + 2];
+					float y = -(float)packet.Data[i * 3 + 3];
+					float z = (float)packet.Data[i * 3 + 4];
+//					Debug.Log(x + " " + y + " " + z);
+					list.Add(new Vector3(x, y, z));
+				}
+				faceMesh.GetComponent<FaceManager>().SetMesh(list);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/FaceManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FaceManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetMesh(List<Vector3> list) {
+		var mesh = new Mesh();
+		mesh.name = "Mesh";
+
+		int numVerts = list.Count;
+
+		var vertices = new List<Vector3>();
+		var normals = new List<Vector3>();
+		var uvs = new List<Vector2>();
+		var triangles = new List<int>();
+
+		foreach (var vertex in list) {
+			vertices.Add(vertex);
+		}
+
+		mesh.vertices = vertices.GetRange(0, numVerts).ToArray();
+//		mesh.normals = normals.GetRange(0, numVerts).ToArray();
+//		mesh.uv = uvs.GetRange(0, numVerts).ToArray();
+//		mesh.triangles = triangles.GetRange(0, numIndices).ToArray();
+		mesh.RecalculateBounds();
+		
+		// not separated at first
+		GetComponent<MeshFilter>().sharedMesh = mesh;
+	}
+}
